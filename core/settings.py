f854bafcca91ef61b5f4ee42a96c51e3221301c6
ilyakochankov <ilyakochankov@yandex.ru>
2021-11-17 07:04:35 +0300

    fix .env path

--- core/settings.py
+++ core/settings.py
@@ -3,11 +3,10 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
+env.read_env(".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env.read_env(str(BASE_DIR.parent.joinpath(".env")))
 
 PROJECT_NAME = "Vendor"
 DB_URL = env("VENDOR_DB_URL")
