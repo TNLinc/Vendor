a994af0447ce4fbc4407876c74a4beafa9f74244
Restyled.io <commits@restyled.io>
2021-11-22 08:59:03 +0000

    Restyled by yapf

--- core/config.py
+++ core/config.py
@@ -6,7 +6,9 @@ from dynaconf import LazySettings
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
 settings: LazySettings = Dynaconf(
-    settings_files=[BASE_DIR / "core/settings.yaml", BASE_DIR / "core/.secrets.yaml"],
+    settings_files=[
+        BASE_DIR / "core/settings.yaml", BASE_DIR / "core/.secrets.yaml"
+    ],
     environments=True,
     load_dotenv=True,
     dotenv_path=BASE_DIR / ".env",
