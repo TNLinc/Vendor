4617c6d96c07b7a1b366674139b4a87c22c50fb9
ilyakochankov <ilyakochankov@yandex.ru>
2021-11-22 11:24:22 +0300

    fix linters

--- core/config.py
+++ core/config.py
@@ -1,14 +1,11 @@
 from pathlib import Path
 
-from dynaconf import Dynaconf
-from dynaconf import LazySettings
+from dynaconf import Dynaconf, LazySettings
 
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
 settings: LazySettings = Dynaconf(
-    settings_files=[
-        BASE_DIR / "core/settings.yaml", BASE_DIR / "core/.secrets.yaml"
-    ],
+    settings_files=[BASE_DIR / "core/settings.yaml", BASE_DIR / "core/.secrets.yaml"],
     environments=True,
     load_dotenv=True,
     dotenv_path=BASE_DIR / ".env",
--- models.py
+++ models.py
@@ -1,17 +1,14 @@
 import enum
+from typing import List, Optional
 import uuid
-from typing import List
-from typing import Optional
 
-import numpy as np
-import sqlalchemy as sa
 from PIL import ImageColor
+import numpy as np
 from pydantic import validator
 from pydantic.color import Color
+import sqlalchemy as sa
 from sqlalchemy import Enum
-from sqlmodel import Field
-from sqlmodel import Relationship
-from sqlmodel import SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 from core.config import settings
 
@@ -47,8 +44,7 @@ class ProductType(enum.Enum):
 
 class ProductBase(SQLModel):
     name: str
-    type: ProductType = Field(sa_column=sa.Column(Enum(ProductType)),
-                              nullable=False)
+    type: ProductType = Field(sa_column=sa.Column(Enum(ProductType)), nullable=False)
     color: str = Field(nullable=False)
 
     @validator("color")
@@ -68,8 +64,7 @@ class Product(ProductBase, table=True):
         target_color = np.array(target_color.as_rgb_tuple())
         product_color = np.array(ImageColor.getrgb(self.color))
         rm = 0.5 * (target_color[0] + product_color[0])
-        return sum(
-            (2 + rm, 4, 3 - rm) * (target_color[:3] - product_color)**2)**0.5
+        return sum((2 + rm, 4, 3 - rm) * (target_color[:3] - product_color) ** 2) ** 0.5
 
     class Config:
         schema_extra = {
