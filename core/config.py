62a96cb8f0166fd582a49db42cd655d59bd935e7
Ilya Kochankov <fakafafakafa@mail.ru>
2021-11-22 14:22:03 +0300

    fix tests ci/cd (#4)
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    * Restyled by whitespace
    
    * Restyled by yapf
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by prettier-yaml
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by prettier-yaml
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * fix tests ci/cd
    
    * fix tests ci/cd
    
    * fix linters
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by prettier-markdown
    
    * Restyled by prettier-yaml
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * fix linters
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    * Restyled by black
    
    * Restyled by isort
    
    * fix linters
    
    * fix linters
    
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ core/config.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from dynaconf import Dynaconf, LazySettings
+
+BASE_DIR: Path = Path(__file__).resolve().parent.parent
+
+settings: LazySettings = Dynaconf(
+    settings_files=[BASE_DIR / "core/settings.yaml", BASE_DIR / "core/.secrets.yaml"],
+    environments=True,
+    load_dotenv=True,
+    dotenv_path=BASE_DIR / ".env",
+    envvar_prefix=False,
+)
