fix distance calculation (#12)
* fix distance calculation

* black format

Co-authored-by: Restyled.io <commits@restyled.io>

models.py
@@ -1,4 +1,5 @@
 import enum
+from math import sqrt
 from typing import List, Optional
 import uuid
 
@@ -64,7 +65,12 @@ class Product(ProductBase, table=True):
         target_color = np.array(target_color.as_rgb_tuple())
         product_color = np.array(ImageColor.getrgb(self.color))
         rm = 0.5 * (target_color[0] + product_color[0])
-        return sum((2 + rm, 4, 3 - rm) * (target_color[:3] - product_color) ** 2) ** 0.5
+        return sqrt(
+            sum(
+                (2 + rm / 256, 4, 2 + (255 - rm) / 256)
+                * (target_color[:3] - product_color) ** 2
+            )
+        )
 
     class Config:
         schema_extra = {